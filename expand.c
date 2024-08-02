5d52c9371e853f0da057a0170de705b8b3f20ca4
bajjishaimae <150949430+bajjishaimae@users.noreply.github.com>
2024-08-02 18:31:01 +0100

    add functions for expanding

--- expand.c
+++ expand.c
@@ -1,2 +1,53 @@
 #include "minishell.h"
 
+int can_expand(char *input)
+{
+    int d_quote;
+    int s_quote_index;
+    int d_quote_index;
+    int i = 0;
+
+    d_quote = -1;
+    while (input[i])
+    {
+        if (input[i] == '"')
+        {
+            d_quote = -d_quote;
+            d_quote_index = i;
+        }
+        else if (input[i] == '\'')
+            s_quote_index = i;
+        if (input[i] == '$' && d_quote_index < s_quote_index && d_quote > 0)
+            return (1);
+        i++;
+    }
+    return (0);
+}
+
+char *variable_name(char *input)
+{
+    int len = 0;
+    int start = 0;
+
+    if (input[start] == '$') 
+        start++;
+    while (input[start + len] && (isalnum(input[start + len]) || input[start + len] == '_')) 
+        len++;
+    char *var_name = malloc(len + 1);
+    if (var_name) {
+        strncpy(var_name, input + start, len);
+        var_name[len] = '\0';
+    }
+    return var_name;
+}
+
+char *get_value(char **env_vars)
+{
+    
+}
+
+
+
+
+
+
